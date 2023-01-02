11486958afeb8b164273670282b55be54bcd9098
sunyctf <changtengfei888@163.com>
2023-01-02 10:08:30 +0800

    :lipstick:

--- js+css3全屏2023新年快乐动画特效/js/script.js
+++ js+css3全屏2023新年快乐动画特效/js/script.js
@@ -15,9 +15,13 @@ const stars = () => {
     
     const duration = Math.random() * 2;
     
+    // 设置持续时间
+    // js中除了减法计算之外，不允许随便写-。因为会混淆。所以，DOM标准规定，所有带-的css属性名，一律去横线变驼峰
+    // css属性animation-duration,在js中改写为驼峰形式：animationDuration
     star.style.animationDuration = 2 + duration + 's';
+    // 设置延迟 
     star.style.animationDelay = 2 + duration + 's';
-    
+    // 把新创建的节点追加到父元素下所有直接子元素的结尾
     section.appendChild(star);
     i++;
   }
